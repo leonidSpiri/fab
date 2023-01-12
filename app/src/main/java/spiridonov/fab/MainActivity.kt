9da93ee03cedec2961a6ebc8c5d8d2a23a955c4f
leonidSpiri <2003.leonid2003@gmai.com>
2023-01-12 16:19:05 +0300

    all works good

--- app/src/main/java/spiridonov/fab/MainActivity.kt
+++ app/src/main/java/spiridonov/fab/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import spiridonov.fab.databinding.ActivityMainBinding
 
 
@@ -17,36 +19,51 @@ class MainActivity : AppCompatActivity() {
 
     private var isAllFabsVisible = false
 
+    private val fabsPairsList = mutableListOf<Pair<View, View>>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initFabs()
         createFab()
+        btnClickListener()
+    }
+
+    private fun btnClickListener() =
+        binding.btnResetMenu.setOnClickListener {
+            initFabs()
+        }
+
+    private fun initFabs() {
+        fabsPairsList.clear()
+        with(binding) {
+            fabsPairsList.add(Pair(historyFab, historyActionText))
+            fabsPairsList.add(Pair(searchFab, searchActionText))
+            fabsPairsList.add(Pair(homeFab, homeActionText))
+            fabsPairsList.add(Pair(settingsFab, settingsActionText))
+        }
+        fabsPairsList.forEach { pair ->
+            setupSwipeListener(pair.first, pair.second)
+            if (isAllFabsVisible) {
+                showOut(pair.first)
+                showOut(pair.second)
+            }
+        }
+        isAllFabsVisible = false
     }
 
     private fun createFab() = with(binding) {
         floatingActionButton.setOnClickListener {
-
-            if (!isAllFabsVisible) {
-                showIn(historyFab)
-                showIn(historyActionText)
-                showIn(searchFab)
-                showIn(searchActionText)
-                showIn(homeFab)
-                showIn(homeActionText)
-                showIn(settingsFab)
-                showIn(settingsActionText)
-                floatingActionButton.extend()
-            } else {
-                showOut(historyFab)
-                showOut(historyActionText)
-                showOut(searchFab)
-                showOut(searchActionText)
-                showOut(homeFab)
-                showOut(homeActionText)
-                showOut(settingsFab)
-                showOut(settingsActionText)
-                floatingActionButton.shrink()
-            }
+            if (!isAllFabsVisible)
+                fabsPairsList.forEach { pair ->
+                    showIn(pair.first)
+                    showIn(pair.second)
+                }
+            else
+                fabsPairsList.forEach { pair ->
+                    showOut(pair.first)
+                    showOut(pair.second)
+                }
             isAllFabsVisible = !isAllFabsVisible
         }
     }
@@ -57,7 +74,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showOut(v: View) {
-        v.visibility = View.GONE
         v.startAnimation(toBottom)
+        v.visibility = View.GONE
+    }
+
+    private fun setupSwipeListener(fab: View, textView: View) {
+        fab.setOnTouchListener(object : OnSwipeTouchListener(this@MainActivity) {
+            override fun onSwipeLeft() {
+                super.onSwipeLeft()
+                showOut(fab)
+                showOut(textView)
+                fabsPairsList.remove(Pair(fab, textView))
+            }
+
+            override fun onSwipeRight() {
+                super.onSwipeRight()
+                showOut(fab)
+                showOut(textView)
+                fabsPairsList.remove(Pair(fab, textView))
+            }
+        })
     }
 }
