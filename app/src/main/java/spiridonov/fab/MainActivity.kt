36a8f840e9aee8d779ddf0179899761f738fde24
leonidSpiri <2003.leonid2003@gmai.com>
2023-01-12 18:17:45 +0300

    swipe works better

--- app/src/main/java/spiridonov/fab/MainActivity.kt
+++ app/src/main/java/spiridonov/fab/MainActivity.kt
@@ -1,11 +1,13 @@
 package spiridonov.fab
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
-import android.view.animation.AnimationUtils
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
 import spiridonov.fab.databinding.ActivityMainBinding
 
 
@@ -14,8 +16,6 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    private val fromBottom by lazy { AnimationUtils.loadAnimation(this, R.anim.anim_from_bottom) }
-    private val toBottom by lazy { AnimationUtils.loadAnimation(this, R.anim.anim_to_bottom) }
 
     private var isAllFabsVisible = false
 
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initFabs()
-        createFab()
+        fabsListener()
         btnClickListener()
     }
 
@@ -45,54 +45,75 @@ class MainActivity : AppCompatActivity() {
         fabsPairsList.forEach { pair ->
             setupSwipeListener(pair.first, pair.second)
             if (isAllFabsVisible) {
-                showOut(pair.first)
-                showOut(pair.second)
+                animShowOut(pair.first)
+                animShowOut(pair.second)
             }
+            pair.first.visibility = View.GONE
+            pair.second.visibility = View.GONE
         }
         isAllFabsVisible = false
     }
 
-    private fun createFab() = with(binding) {
+    private fun fabsListener() = with(binding) {
         floatingActionButton.setOnClickListener {
             if (!isAllFabsVisible)
                 fabsPairsList.forEach { pair ->
-                    showIn(pair.first)
-                    showIn(pair.second)
+                    animShowIn(pair.first)
+                    animShowIn(pair.second)
                 }
             else
                 fabsPairsList.forEach { pair ->
-                    showOut(pair.first)
-                    showOut(pair.second)
+                    println(fabsPairsList.size)
+                    print(((pair.second) as TextView).text)
+                    animShowOut(pair.first)
+                    animShowOut(pair.second)
                 }
             isAllFabsVisible = !isAllFabsVisible
         }
     }
 
-    private fun showIn(v: View) {
+    private fun animShowIn(v: View) {
         v.visibility = View.VISIBLE
-        v.startAnimation(fromBottom)
+        v.alpha = 0f
+        v.translationY = v.height.toFloat()
+        v.animate()
+            .setDuration(200)
+            .setListener(object : AnimatorListenerAdapter() {
+            })
+            .translationY(0F)
+            .alpha(1f)
+            .start()
     }
 
-    private fun showOut(v: View) {
-        v.startAnimation(toBottom)
-        v.visibility = View.GONE
+    private fun animShowOut(v: View) {
+        v.alpha = 1f
+        v.translationY = 0F
+        v.animate()
+            .setDuration(200)
+            .translationY(v.height.toFloat())
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    v.visibility = View.GONE
+                    super.onAnimationEnd(animation)
+                }
+            }).alpha(0f)
+            .start()
     }
 
-    private fun setupSwipeListener(fab: View, textView: View) {
+    private fun setupSwipeListener(fab: View, textView: View) =
         fab.setOnTouchListener(object : OnSwipeTouchListener(this@MainActivity) {
             override fun onSwipeLeft() {
                 super.onSwipeLeft()
-                showOut(fab)
-                showOut(textView)
+                animShowOut(fab)
+                animShowOut(textView)
                 fabsPairsList.remove(Pair(fab, textView))
             }
 
             override fun onSwipeRight() {
                 super.onSwipeRight()
-                showOut(fab)
-                showOut(textView)
+                animShowOut(fab)
+                animShowOut(textView)
                 fabsPairsList.remove(Pair(fab, textView))
             }
         })
-    }
 }
